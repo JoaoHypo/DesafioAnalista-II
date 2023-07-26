aea95753ab4ccfe4954650715ea6e3e67e017f5d
Hypo - Unb <jguibarboza@gmail.com>
2023-07-26 13:52:44 -0300

    Funcional e mais polido

--- www/api.php
+++ www/api.php
@@ -3,21 +3,19 @@ $publicKey = '57d0121bf03abe102f19923088ea530f';
 $privateKey = 'bfb635effc262315cd77187c4f21637999cb2968';
 $baseUrl = 'https://gateway.marvel.com/v1/public/characters';
 
-// IDs dos heróis específicos (Homem-Aranha, Storm e Tocha Humana)
-$heroIds = array(1009610, 1009629, 1009351); // Esses IDs são específicos para Homem-Aranha, Storm e Tocha Humana
+// Função para buscar um herói pelo nome e retornar suas informações
+function getHeroByName($name)
+{
+    global $publicKey, $privateKey, $baseUrl;
 
-// Criando o timestamp (ts) atual
-$timestamp = time();
+    // Criando o timestamp (ts) atual
+    $timestamp = time();
 
-// Construindo o hash usando o timestamp, a private key e a public key
-$hash = md5($timestamp . $privateKey . $publicKey);
+    // Construindo o hash usando o timestamp, a private key e a public key
+    $hash = md5($timestamp . $privateKey . $publicKey);
 
-// Variável para armazenar os dados dos heróis
-$heroesData = array();
-
-foreach ($heroIds as $heroId) {
-    // Montando a URL completa com os parâmetros de autenticação e o ID do herói
-    $url = "{$baseUrl}/{$heroId}?ts={$timestamp}&apikey={$publicKey}&hash={$hash}";
+    // Montando a URL completa com os parâmetros de autenticação e o nome do herói
+    $url = "{$baseUrl}?name={$name}&ts={$timestamp}&apikey={$publicKey}&hash={$hash}";
 
     // Realizando a requisição à API usando cURL
     $ch = curl_init();
@@ -31,10 +29,63 @@ foreach ($heroIds as $heroId) {
 
     // Verificando se há dados na resposta da API
     if (isset($data['data']['results'][0])) {
-        $heroesData[] = $data['data']['results'][0];
+        return $data['data']['results'][0];
+    } else {
+        return null;
     }
 }
 
-// Retornando os dados dos heróis para serem exibidos no index.php
-return $heroesData;
+// Função para obter as histórias de um herói pelo ID
+function getHeroStories($heroId)
+{
+    global $publicKey, $privateKey, $baseUrl;
+
+    // Criando o timestamp (ts) atual
+    $timestamp = time();
+
+    // Construindo o hash usando o timestamp, a private key e a public key
+    $hash = md5($timestamp . $privateKey . $publicKey);
+
+    // Montando a URL completa com os parâmetros de autenticação e o ID do herói
+    $url = "{$baseUrl}/{$heroId}/stories?ts={$timestamp}&apikey={$publicKey}&hash={$hash}";
+
+    // Realizando a requisição à API usando cURL
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    // Decodificando a resposta JSON
+    $data = json_decode($response, true);
+
+    // Verificando se há dados na resposta da API
+    if (isset($data['data']['results'])) {
+        // Embaralhar as histórias para selecionar 5 aleatórias
+        shuffle($data['data']['results']);
+        $selectedStories = array_slice($data['data']['results'], 0, 5);
+        return $selectedStories;
+    } else {
+        return [];
+    }
+}
+
+// Obtendo os heróis da Marvel pelos nomes listados
+$heroesNames = array('Hulk', 'Storm', 'Daredevil');
+$selectedHeroesData = array();
+
+foreach ($heroesNames as $heroName) {
+    $hero = getHeroByName($heroName);
+
+    if ($hero !== null) {
+        $heroId = $hero['id'];
+        $selectedHeroesData[] = [
+            'name' => $hero['name'],
+            'thumbnail' => "{$hero['thumbnail']['path']}.{$hero['thumbnail']['extension']}",
+            'stories' => getHeroStories($heroId)
+        ];
+    }
+}
+
+return $selectedHeroesData;
 ?>
--- www/index.php
+++ www/index.php
@@ -5,58 +5,44 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php include 'header.php'; ?>
-    
     <h1>Meus Heróis Favoritos da Marvel</h1>
 
-    <?php
-    // Incluindo api.php para obter os dados dos heróis
-    $heroes = include 'api.php';
+    <div class="container">
+        <?php
+        // Incluindo api.php para obter os dados dos heróis
+        $heroes = include 'api.php';
 
-    // Verificando se há heróis disponíveis
-    if (count($heroes) > 0) {
-        // Exibindo os dados dos heróis e suas histórias
-        foreach ($heroes as $hero) {
-            $name = $hero['name'];
-            $thumbnailUrl = "{$hero['thumbnail']['path']}.{$hero['thumbnail']['extension']}";
+        // Verificando se há heróis disponíveis
+        if (count($heroes) > 0) {
+            // Exibindo os dados dos heróis e suas histórias
+            foreach ($heroes as $hero) {
+                $name = $hero['name'];
+                $thumbnailUrl = $hero['thumbnail'];
 
-            // Exibindo o nome e a imagem do herói
-            echo "<div class='hero-card'>";
-            echo "<h2>{$name}</h2>";
-            echo "<img src='{$thumbnailUrl}' alt='{$name}'>";
+                // Exibindo o nome e a imagem do herói
+                echo "<div class='hero-card'>";
+                echo "<h2>{$name}</h2>";
+                echo "<img src='{$thumbnailUrl}' alt='{$name}'>";
 
-            // Obtendo as histórias do herói
-            $characterId = $hero['id'];
-            $storiesUrl = "https://gateway.marvel.com/v1/public/characters/{$characterId}/stories?ts={$timestamp}&apikey={$publicKey}&hash={$hash}";
-
-            $ch_stories = curl_init();
-            curl_setopt($ch_stories, CURLOPT_URL, $storiesUrl);
-            curl_setopt($ch_stories, CURLOPT_RETURNTRANSFER, true);
-            $response_stories = curl_exec($ch_stories);
-            curl_close($ch_stories);
-
-            $data_stories = json_decode($response_stories, true);
-
-            // Verificando se há histórias associadas ao herói
-            if (isset($data_stories['data']['results']) && count($data_stories['data']['results']) > 0) {
-                echo "<ul>";
-                $stories = array_slice($data_stories['data']['results'], 0, 5); // Exibir apenas as 5 primeiras histórias
-                foreach ($stories as $story) {
-                    $storyTitle = $story['title'];
-                    echo "<li>{$storyTitle}</li>";
+                // Verificando se há histórias associadas ao herói
+                if (isset($hero['stories']) && count($hero['stories']) > 0) {
+                    echo "<ul>";
+                    echo "<h2>5 Histórias presente:</h2>";
+                    foreach ($hero['stories'] as $story) {
+                        $storyTitle = $story['title'];                        
+                        echo "<li>{$storyTitle}</li>";
+                    }
+                    echo "</ul>";
+                } else {
+                    echo "<p>Nenhuma história encontrada para este herói.</p>";
                 }
-                echo "</ul>";
-            } else {
-                echo "<p>Nenhuma história encontrada para este herói.</p>";
+
+                echo "</div>";
             }
-
-            echo "</div>";
+        } else {
+            echo "<p>Nenhum herói encontrado.</p>";
         }
-    } else {
-        echo "<p>Nenhum herói encontrado.</p>";
-    }
-    ?>
-
-    <?php include 'footer.php'; ?>
+        ?>
+    </div>
 </body>
 </html>
